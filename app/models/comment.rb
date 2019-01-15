3aad7aedf8bd7f7a0aca6244ab02b588eb626def
Tommy <t@tommyhoang.com>
2019-01-14 19:03:30 -0800

    add associations between comment post and user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
     validates :body, presence: true, length: { minimum: 10 }
     validates :user_id, presence: true
     validates :post_id, presence: true
+
+    belongs_to :post
+    belongs_to :user
 end
